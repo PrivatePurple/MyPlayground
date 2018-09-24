05b6fb7ef111e299c3e06231e52385fce47701c5
Keenan <Keenanking555@gmail.com>
2018-09-24 13:01:16 -0600

    Initial commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+
+var firstString = "Hello world! ~🦑"
+
+var string = "Wow, so this is swift. Seems pretty neat. I like it so far. Javascript but a lot more, how do you say... Ah, Streamlined."
+print(firstString)

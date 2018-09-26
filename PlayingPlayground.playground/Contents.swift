dbe339a1f5ad5a8e824084230a1cde670ad40fc5
Keenan <Keenanking555@gmail.com>
2018-09-26 12:28:17 -0600

    added some more tests

--- PlayingPlayground.playground/Contents.swift
+++ PlayingPlayground.playground/Contents.swift
@@ -12,3 +12,32 @@ if let name = optionalName {
     greeting = "\(name), You're a kid now, you're a squid now!"
 }
 
+
+func greet(_ person: String, on day: String) -> String {
+    return "Hello \(person), today is \(day)."
+}
+greet("John", on: "Wednesday")
+
+//
+
+func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+    var min = scores[0]
+    var max = scores[0]
+    var sum = 0
+    
+    for score in scores {
+        if score > max {
+            max = score
+        } else if score < min {
+            min = score
+        }
+            sum += score
+    }
+    
+    return (min, max, sum)
+}
+let statistics = calculateStatistics(scores: [5, 3, 100, 3, 9])
+print(statistics.sum)
+//Prints "120"
+print(statistics.2)
+//Prints "120"
